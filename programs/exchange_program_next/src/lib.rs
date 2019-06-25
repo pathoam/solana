0c46b4cf56198678923357bb5649377a93eb7f95
pat <patrick@solana.com>
2019-06-25 15:10:20 -0600

    update program/api lib names, deps

--- programs/exchange_program_next/src/lib.rs
+++ programs/exchange_program_next/src/lib.rs
@@ -1,12 +1,12 @@
 #[macro_export]
-macro_rules! solana_exchange_program {
+macro_rules! solana_exchange_program_next {
     () => {
         (
-            "solana_exchange_program".to_string(),
-            solana_exchange_api::id(),
+            "solana_exchange_program_next".to_string(),
+            solana_exchange_api_next::id(),
         )
     };
 }
-use solana_exchange_api::exchange_processor::process_instruction;
+use solana_exchange_api_next::exchange_processor::process_instruction;
 
 solana_sdk::solana_entrypoint!(process_instruction);
